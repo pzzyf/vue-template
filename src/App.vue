aaaa2335d8cd5e91eacc114f0f6cb0272bdff265
afe1 <yunfei.zhu@nwowtec.com>
2024-07-16 14:01:23 +0800

    feat

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
-<script setup>
-import a from '@/components/1';
-const count = ref(0)
-</script>
-
 <template>
-  <div class="font-size-8">{{ count }}</div>
-  <HelloWorld />
-  {{ a }}
+  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <RouterView />
+  </main>
 </template>
-
-<style scoped>
-</style>
